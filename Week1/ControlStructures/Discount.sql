37fb47248b1ee4c1888aca8d562f735bf7071ca4
Sankari M <sankarimothilal@gmail.com>
2026-06-30 15:33:42 +0530

    Add Week1 assignments

---
+++ Week1/ControlStructures/Discount.sql
@@ -0,0 +1,13 @@
+DECLARE
+BEGIN
+    FOR c IN (SELECT CustomerID, Age FROM Customers) LOOP
+        IF c.Age > 60 THEN
+            UPDATE Loans
+            SET InterestRate = InterestRate - 1
+            WHERE CustomerID = c.CustomerID;
+        END IF;
+    END LOOP;
+
+    COMMIT;
+END;
+/
