abf4b5312fabfade4a717a0cb944f04c9d58a49b
Sankari M <sankarimothilal@gmail.com>
2026-07-09 18:11:38 +0530

    Add Week3 assignments

---
+++ Week3/spring-learn/src/main/java/com/cognizant/spring_learn/controller/AuthenticationController.java
@@ -0,0 +1,13 @@
+package com.cognizant.spring_learn.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthenticationController {
+
+    @GetMapping("/authenticate")
+    public String authenticate() {
+        return "Authentication Successful";
+    }
+}
